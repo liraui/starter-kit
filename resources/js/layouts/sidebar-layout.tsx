ce63749d055905e09298d4ca942960592e71da65
Ahsan Zia <15082582+ziaahsan@users.noreply.github.com>
2025-11-17 15:55:56 -0500

    add empty state, fix border

--- resources/js/layouts/sidebar-layout.tsx
+++ resources/js/layouts/sidebar-layout.tsx
@@ -14,7 +14,7 @@ function SidebarLayout({ children, breadcrumbs }: SidebarLayoutProps) {
                 <AppSidebar />
                 <SidebarInset>
                     <div className="bg-sidebar fixed top-12 right-0 left-0 h-12 w-full"></div>
-                    <div className="bg-muted border-border/50 sticky top-12 rounded-t-xl border-b px-8 dark:bg-[#0d0d0d]">
+                    <div className="bg-muted border-border/50 sticky top-12 z-999 rounded-t-xl border-b px-8 dark:bg-[#0d0d0d]">
                         <div className="flex h-16 w-full shrink-0 items-center gap-2">
                             <SidebarTrigger className="-ml-1" />
                             <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,7 +1,34 @@
+import { Button } from '@/components/ui/button';
+import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '@/components/ui/empty';
+import { Logo } from '@/components/ui/logo';
 import { DashboardShell } from '@/layouts/dashboard-shell';
+import { ArrowUpRightIcon } from 'lucide-react';
 
 export default function Dashboard() {
-    return <div>{/*  */}</div>;
+    return (
+        <div className="flex w-full flex-col gap-8 px-8 py-8">
+            <Empty className="bg-background/50 relative mx-auto flex h-full w-full items-center justify-center">
+                <EmptyHeader>
+                    <EmptyMedia variant="icon">
+                        <Logo />
+                    </EmptyMedia>
+                    <EmptyTitle>You're all caught up</EmptyTitle>
+                    <EmptyDescription>Need help getting started? Check out our documentation to learn more about Lira.</EmptyDescription>
+                </EmptyHeader>
+                <span className='text-sm'>
+                    Go to
+                    <Button variant="link" asChild className="text-muted-foreground" size="sm">
+                        <span>
+                            <a href="https://liraui.com" className="text-primary">
+                                liraui.com
+                            </a>
+                            <ArrowUpRightIcon />
+                        </span>
+                    </Button>
+                </span>
+            </Empty>
+        </div>
+    );
 }
 
 Dashboard.layout = (page: React.ReactNode) => (
